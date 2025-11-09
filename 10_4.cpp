55d3ceeb0e24048f57064fa5fcb260a65cc37ce1
NishadPatil008 <nishadpatil2008@gmail.com>
2025-11-09 17:20:25 +0530

    b

--- 10_4.cpp
+++ 10_4.cpp
@@ -6,25 +6,54 @@ class Stack {
     T arr[10];
     int top;
 public:
-    Stack() 
-    { top = -1; }
+    Stack() { top = -1; }
+
     void push(T val) {
         if (top < 9)
             arr[++top] = val;
-    }
-    T pop() {
-        if (top == -1)
-            return 0;
         else
+            cout << "Stack Overflow!" << endl;
+    }
+
+    T pop() {
+        if (top == -1) {
+            cout << "Stack Underflow!" << endl;
+            return 0;
+        } else {
             return arr[top--];
+        }
     }
 };
 
 int main() {
     Stack<int> s;
-    s.push(10);
-    s.push(20);
-    cout << s.pop()<< endl;
-    cout << s.pop()<< endl;
+    int ch;
+
+    while (1) {
+        cout << "\n1. Push\n2. Pop\n3. Exit\nEnter choice: ";
+        cin >> ch;
+
+        switch (ch) {
+            case 1: {
+                int val;
+                cout << "Enter value to push: ";
+                cin >> val;
+                s.push(val);
+                break;
+            }
+            case 2: {
+                int popped = s.pop();
+                if (popped != 0)
+                    cout << "Popped: " << popped << endl;
+                break;
+            }
+            case 3:
+                cout << "Exiting..." << endl;
+                return 0;
+            default:
+                cout << "Invalid choice!" << endl;
+        }
+    }
+
     return 0;
 }
